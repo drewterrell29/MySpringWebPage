9e22395ae3fbc6968f52756569281925e14a7e4f
Drew Terrell <drewterrell29@gmail.com>
2016-01-28 12:37:49 -0600

    Init commit

---
+++ src/main/java/com/terrell/MySpringWebPageApplication.java
@@ -0,0 +1,12 @@
+package com.terrell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringWebPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringWebPageApplication.class, args);
+	}
+}
